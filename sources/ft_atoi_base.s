ff7f890f02d0ed0aadbcf96628771373d55c2f65
Victor Martinez estrada <vicmarti@c1r3s4.42madrid.com>
2021-04-27 15:41:06 +0200

    Progress on atoibase, aux functs to verify validity of base.

--- sources/ft_atoi_base.s
+++ sources/ft_atoi_base.s
@@ -1,24 +1,55 @@
 	global	_ft_atoi_base
-	section	.data
+	extern	_ft_isspace
+	extern	_ft_strchr
 
 	section	.text
 _ft_atoi_base:
-	xor		rax,rax
 	cmp		rdi,0
-	mov		r10,-1
-	jz		.DONE
-.IS_SPACE:
-	inc		r10
-	cmp		byte [rdi + r10],0x20
-	cmp		byte [rdi + r10],0x09
-	jl		.IS_SIGN
-	cmp		byte [rdi + r10],0x0d
-	jle		.IS_SPACE
-.IS_SIGN
-	cmp:W
+	jz		.DONE_ERROR
+	cmp		rsi,0
+	jz		.DONE_ERROR
+	cmp		byte [rsi],0
+	jz		.DONE_ERROR
+	cmp		byte [rsi+1],0
+	jz		.DONE_ERROR
+	;base_validation
+	mov		r11,rdi		;*base_iter -> base_len
+	mov		r10,rdi		;*base -> outval
+	mov		r9,rsi		;*str bkp
+.NEXT_BASE_CHAR:
+	mov		byte rdi,[r11]
+	cmp		rdi,0x2b
+	jz		.DONE_ERROR
+	cmp		rdi,0x2d
+	jz		.DONE_ERROR
+	call	_ft_isspace
+	cmp		rax,0
+	jz		.DONE_ERROR
+	mov		byte rsi,rdi
+	inc		r11
+	mov		rsi,r11
+	call	_ft_strchr
+	cmp		rax,0
+	jnz		.DONE_ERROR
+	;assignment proper
+	sub		r11,r10		;base_len
+.SKIP_SPACES
+	mov		rdi,[r9]
+	inc		r9
+	call	_ft_isspace
+	cmp		rax,0
+	jz		.SKIP_SPACES
+.SIGN_MNGMNT
+	mov		rdi,[r9]
+	inc		r9
+	cmp		rdi,0x2b
+	jz		.SIGN_MNGMNT
+	cmp		rdi,0x2d
+	jnz		.ASSIGN
 
-	
 
-
+.DONE_ERROR:
+	xor		r11,r11
 .DONE:
+	mov		rax,r11
 	ret
---
+++ sources/ft_isspace.s
@@ -0,0 +1,15 @@
+	global	_ft_isspace
+	section	.text
+_ft_isspace
+	cmp		edi,0x20
+	jz		.RET_TRUE
+	cmp		edi,0x11
+	jl		.RET_FALSE
+	cmp		edi,0x15
+	jg		.RET_FALSE
+.RET_TRUE
+	mov		eax,1
+	ret
+.RET_FALSE
+	xor		eax,eax
+	ret
